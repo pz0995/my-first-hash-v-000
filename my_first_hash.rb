4a1f180015541ca99dc873e81d6271989fd4ae60
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-21 08:22:11 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,9 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+my_hash = Hash.new
+my_hash.store("red", "pepperoni")
+my_hash.store("green", "grass")
+my_hash.store("blue", "sky")
   
 end
 
